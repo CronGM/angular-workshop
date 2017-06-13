9e7b7e57e97f39cb4174127f432a758ef4df6411
Garcia Mendoza <c.garcia.mendoza@accenture.com>
2017-06-13 00:18:06 -0500

    Bootstrapping and Basic Controllers

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,21 @@
 // Here's where the magic will happen
-;
+// REMEMBER: Declare a module, two parameters (name, dependencies)
+angular.module('app', []);
+
+// Get module, one parameter (module_name)
+angular.module('app')
+.run(function($rootScope) {		// run method executes code when initializing Angular 
+	// The base scope for the entire Angular app is called $rootScope
+	// $rootScope must be added as a dependency to use
+	$rootScope.default = 'Hello from Rootscope!';
+});
+
+angular.module('app')
+.controller('myCtrl', function($scope) {	// Create controller: two parameters (controller_name, controller_function)
+	// Angular controllers need a scope to work with
+	$scope.favNum = 42;
+})
+.controller('otherCtrl', function($scope) {
+	$scope.favNum = '007';
+	$scope.default = 'Hola from Controller';
+});
