4b2cface34f3bc952ca754405d998874143657bd
Garcia Mendoza <c.garcia.mendoza@accenture.com>
2017-06-14 01:01:08 -0500

    Refactor controller declarations for best practices

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,12 @@ angular.module('app', []);
 
 // Get module, one parameter (module_name)
 angular.module('app')
-.run(function($rootScope) {		// run method executes code when initializing Angular 
+.run(['$rootScope', function($rootScope) {		// run method executes code when initializing Angular 
 	// The base scope for the entire Angular app is called $rootScope
 	// $rootScope must be added as a dependency to work correctly
+	
 	$rootScope.default = 'Hello from Rootscope!';
-})
+}]);
 
 // You can add constant and app-wide variables using both the constant and value methods
 // Each has its differences and specific feature, and are used like services (they must be passed as dependencies to work)
@@ -20,17 +21,25 @@ angular.module('app')
 });
 
 angular.module('app')
-.controller('myCtrl', function($scope, usuario) {	// Create controller: two parameters (controller_name, controller_function)
+.controller('myCtrl', myCtrl)
+.controller('otherCtrl', otherCtrl)
+.controller('showHideCtrl', showHideCtrl);
+
+function myCtrl ($scope, usuario) {	// Create controller: two parameters (controller_name, controller_function)
 	// Angular controllers need a scope to work with
 	$scope.favNum = 42;
 	$scope.name = usuario;
-})
-.controller('otherCtrl', function($scope, user) {
+};
+myCtrl.$inject = ['$scope', 'usuario'];
+
+function otherCtrl ($scope, user) {
 	$scope.favNum = '007';
 	$scope.default = 'Hola from Controller';
 	$scope.name = user.first + ' ' + user.last;
-})
-.controller('showHideCtrl', function($scope, user) {
+};
+otherCtrl.$inject = ['$scope', 'user'];
+
+function showHideCtrl ($scope, user) {
 	$scope.favNum = '007';
 	$scope.first = user.first;
 	$scope.last = user.last;
@@ -39,4 +48,5 @@ angular.module('app')
 	$scope.showHideOnClick = function() {
 		$scope.isShow = !$scope.isShow;
 	}
-});
+};
+showHideCtrl.$inject = ['$scope', 'user'];
--- src/js/models.ctrl.js
+++ src/js/models.ctrl.js
@@ -1,11 +1,29 @@
 angular.module('app')
-.controller('modelsCtrl', function($scope) {
+.controller('modelsCtrl', modelsCtrl)
+.controller('extraCtrl', extraCtrl);
+
+/* CONTROLLER BEST PRACTICES
+	* It's a good practice to declare your controller function outside of the controller's own declaration (seen above). This makes the code easier to read.
+	* It's a good practice to always name your functions, as it makes code easier to debug. By following the above practice, your functions will always be named.
+	* It's not only a good practice, but necessary, to inject your dependencies as strings, so they are not lost on code minification.
+*/
+function modelsCtrl ($scope) {
+	// If you are going to use controller aliases, you must add your viewmodel properties to the controller object's instance, instead of adding them to the $scope.
+	// GOOD PRACTICE: assign "this" to a variable that is semantic. In this case we use "vm" for "view-model", but you can use anything you want.
 	var vm = this;
 
+	// $scope.test = 'Hola mundi!';		<--- Don't use this when using controller aliases, or your variables will be lost in the document
 	vm.test = 'Hola mundi!';
-})
-.controller('extraCtrl', function($scope) {
+}
+/*
+Inject the necesarry dependencies as an array of strings, where each item in the array is a dependency to inject.
+This dependencies are the same that are passed to the controller function as parameters
+*/
+modelsCtrl.$inject = ['$scope'];
+
+function extraCtrl ($scope) {
 	var vm = this;
 
-	vm.test = 'Extra extra!';
-});
+	vm.test = 'Extra extri!';
+}
+extraCtrl.$inject = ['$scope'];
